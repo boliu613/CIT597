e14e0f08e6436ec878a25b46faac6e7118422f1c
liubo1678 <boliu@vagvlan536.1409.wlan.wireless-pennnet.upenn.edu>
2015-03-28 17:57:35 -0400

    new project

---
+++ CollegeFinder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollegeFinder::Application.config.secret_token = '3fcf31375d5d5beeda1ef099bce7646011f85c6f84150ef33c0da1e5ea40ef42223b434da0c81537123f51a217f40236455943f89bd1a26770dc9dde76194f98'
